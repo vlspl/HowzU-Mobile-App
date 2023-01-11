aec4f154c55bd2e70021223592cebd23eb4df70a
gauravjaadhav <grj1901@gmail.com>
2023-01-11 15:57:39 +0530

    GET_SLIDER_IMG url change for sloder images

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -34,7 +34,7 @@ module.exports = {
   //https://visionarylifescience.com/Images/APP%20FEATURE2.png
   SILDER_IMG_URL: "https://visionarylifescience.com/",
   GET_SLIDER_IMG:
-    "http://endpoint.visionarylifescience.com/" + AUTH + "GetSliderPath",
+    "https://endpoint.visionarylifescience.com/" + AUTH + "GetSliderPath",
 
   //Helath grphs
 
